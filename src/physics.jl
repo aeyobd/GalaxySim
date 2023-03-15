8ea131629701d35993c37f0483ed6d5032a3d797
Daniel Boyea <aeyobd@gmail.com>
2023-03-15 16:12:41 -0400

    2-body works

--- src/physics.jl
+++ src/physics.jl
@@ -15,28 +15,19 @@ mutable struct Mass
     m::AbstractFloat
 end
 
-function main(dt=100, t_end=100_000)
-    m1 = Mass([0.01, 0, 0], [0, 6.7038e-7, 0], 0.0001)
+function main(dt=10, t_end=500_000)
+    m1 = Mass([0.01, 0, 0], [0, 6.7038e-8, 0], 1)
+    m2 = Mass([-0.01, 0, 0], [0, -6.7038e-8, 0], 1)
+    masses = [m1, m2]
 
-    open("gal.dat", "w") do f
-        write(f, "time\tx1\tx2\tx3\tv1\tv2\ta\n")
-
-        for t in 0:dt:t_end
-            x1 = m1.x[1]
-            x2 = m1.x[2]
-            x3 = m1.x[3]
-
-            v1 = m1.v[1]
-            v2 = m1.v[2]
-            v3 = m1.v[3]
-
-            d = dΦ(m1.x)
-            a1 = d[1]
-            write(f, "$t\t$x1\t$x2\t$x3\t$v1\t$v2\t$a1\n")
-
-            update_particle!(m1, dt)
-        end
+    files = open_files(length(masses))
+    # open_files
+    for t in 0:dt:t_end
+        record_masses(files, masses)
+        update_particles!(masses, dt)
     end
+
+    close_files(files)
 end
 
 
@@ -50,15 +41,50 @@ function Φ(x, masses)
     return s
 end
 
+function open_files(N)
+    files = Vector()
+    for i in 1:N
+        f = open("data/mass$i.dat", "w")
+        println(f, "x1,x2,x3,v1,v2,v3")
+        push!(files, f)
+    end
 
-function dΦ(x)
-    m0 = Mass([0,0,0], [0,0,0], 1)
-    return gradient(x->Φ(x, [m0]), x)
+    return files
+end
+
+function record_masses(files, masses)
+    for (file, mass) in zip(files, masses)
+        x1 = mass.x[1]
+        x2 = mass.x[2]
+        x3 = mass.x[3]
+        v1 = mass.v[1]
+        v2 = mass.v[2]
+        v3 = mass.v[3]
+        write(file, "$x1,$x2,$x3,$v1,$v2,$v3\n")
+    end
+end
+
+function close_files(files)
+    close.(files)
+end
+
+function dΦ(x, others)
+    gradient(x) do x 
+        Φ(x, others)
+    end
 end
 
 function update_particle!(particle, dt)
     particle.x .+= particle.v * dt 
     particle.v .-= dΦ(particle.x)*dt
+end
+
+function update_particles!(particles, dt)
+    for i in 1:length(particles)
+        particles[i].x .+= particles[i].v * dt
+        particles[i].v .-= dΦ(particles[i].x, particles[1:end .!=i])
+    end
+
 end
 
 
